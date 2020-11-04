52df38d654c444f21814c55bb75d4e5f3deed703
victor tarroni <victortarroni@gmail.com>
2020-11-04 14:33:12 -0300

    completado

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ import figma from "./assets/images/icons/figma.png";
 import chris from "./assets/images/Profile/Chris.jpeg";
 import carol from "./assets/images/Profile/Carol.jpeg";
 import victor from "./assets/images/Profile/Victor.jpg";
-import chart  from "./assets/images/tokiens/chart.png";
-import bulb   from "./assets/images/tokiens/bulb.png";
+import chart from "./assets/images/tokiens/chart.png";
+import bulb from "./assets/images/tokiens/bulb.png";
 import pencil from "./assets/images/tokiens/pencil.png";
 import users from "./assets/images/tokiens/users.png";
-
-
 
 const Root = styled.div`
   width: 100vw;
@@ -165,8 +163,7 @@ const Differential = styled.div`
 `;
 
 const DescriptionDifferential = styled.p`
-  text-align: left;
-  width: 440px;
+   width: 440px;
   height: 66px;
   font-weight: normal;
   font-size: 18px;
@@ -175,41 +172,37 @@ const DescriptionDifferential = styled.p`
 `;
 
 const Text = styled.h3`
-  text-align: center;
+  
   font-weight: 600px;
   font-size: 30px;
   width: 198px;
   height: 26px;
   line-height: 45px;
   margin-bottom: 24px;
-
 `;
 
 const DifferentialLogo = styled.div`
-width:100%
-height:auto;
-padding: 10px;
-border-radius: 70%;
-background:rgba(242, 243, 244, 0.05);
-margin-right: 30px;
-
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  width: 90px;
+  height: 90px;
+   border-radius: 50%;
+  background: rgba(242, 243, 244, 0.05);
+  margin-right: 30px;
+  
 `;
 
- const ImageDifferential = styled.img`
- width: 40px;
- align-items: center;
- 
- `;
+const ImageDifferential = styled.img`
+  width: 40px;
+  `;
 
- const ImageDifferential1 = styled.img`
- width: 40px;
- align-items: center;
- 
- `;
 
- const DivSuporte = styled.div ``;
+const DivSuporte = styled.div`
+display: flex;
+flex-direction: column;
 
- 
+`;
 
 // Seção Quem somos -----------
 
@@ -233,13 +226,14 @@ const Card = styled.div`
   background: #e3e4ea;
   border-radius: 4px;
   align-items: center;
-  margin: 56px 80px 40px 80px;
+  margin: 56px 40px 40px 40px;
+  padding: 12px;
 `;
 
 const ProfilePicture = styled.div`
   height: 131px;
   width: 131px;
-  margin: 24px 0;
+  margin: 12px 0 24px 0;
 `;
 
 const ImageProfile = styled.img`
@@ -248,24 +242,18 @@ const ImageProfile = styled.img`
 `;
 
 const DescriptionSkills = styled.p`
-  padding-top: 20px;
-  text-align: center;
+   text-align: center;
   flex-wrap: wrap;
 `;
 
 const Text1 = styled.h3`
-text-align: center;
-font-weight: 600px;
-font-size: 30px;
-width: 198px;
-height: 26px;
-line-height: 45px;
-margin-bottom: 50px;
-flex-wrap: wrap;
-
-
+  text-align: center;
+  font-weight: 600px;
+  font-size: 21px;
+  line-height: 31.5px;
+  margin-bottom: 24px;
+  flex-wrap: wrap;
 `;
-
 
 // Footer -----------
 
@@ -343,64 +331,58 @@ function App() {
         <DifferentialsBox>
           <Differential>
             <DifferentialLogo>
-          <ImageDifferential1 src={pencil} alt="criatividade"/>
-          </DifferentialLogo>
-          <DivSuporte>
-            <Text>Criatividade</Text>
-            
-                   
-             <DescriptionDifferential>
-              É o que envolve nossa paixão por ideias. Criar, imaginar e
-              inventar novas formas de apresentar o seu serviço/produto faz
-              parte do jeito da nossa agência de comunicação ser.
-            </DescriptionDifferential>
-            </DivSuporte>
-            
-          </Differential>
-
-
-          <Differential>
-          <DifferentialLogo>
-            <ImageDifferential src={chart} alt="chart"/>
+              <ImageDifferential src={pencil} alt="criatividade" />
             </DifferentialLogo>
             <DivSuporte>
-            <Text>Resultados</Text>
-            <DescriptionDifferential>
-              A gente trabalha para ver acontecer. E dá retorno, viu? Nosso
-              maior orgulho é acompanhar as conquistas de cada cliente.
-            </DescriptionDifferential>
-            </DivSuporte>
+              <Text>Criatividade</Text>
 
+              <DescriptionDifferential>
+                É o que envolve nossa paixão por ideias. Criar, imaginar e
+                inventar novas formas de apresentar o seu serviço/produto faz
+                parte do jeito da nossa agência de comunicação ser.
+              </DescriptionDifferential>
+            </DivSuporte>
           </Differential>
 
           <Differential>
-          <DifferentialLogo>
-            <ImageDifferential1 src={bulb} alt="bulb" />
+            <DifferentialLogo>
+              <ImageDifferential src={chart} alt="chart" />
             </DifferentialLogo>
             <DivSuporte>
-            <Text>Estratégia</Text>
-            <DescriptionDifferential>
-              Somos estudiosos, sabia?E adoramos planejar. É assim que
-              descobrimos quais os melhores caminhos para alcançar o seu
-              objetivo.
-            </DescriptionDifferential>
+              <Text>Resultados</Text>
+              <DescriptionDifferential>
+                A gente trabalha para ver acontecer. E dá retorno, viu? Nosso
+                maior orgulho é acompanhar as conquistas de cada cliente.
+              </DescriptionDifferential>
             </DivSuporte>
           </Differential>
-
 
           <Differential>
-          <DifferentialLogo>
-            <ImageDifferential src={users} alt="users" />
+            <DifferentialLogo>
+              <ImageDifferential src={bulb} alt="bulb" />
             </DifferentialLogo>
             <DivSuporte>
-            <Text>Integração</Text>
-            <DescriptionDifferential>
-              Juntos conseguimos soluções melhores. Entre uma área e outra, a
-              gente se ajuda e é dessa cooperação que surgem as boas ideias.
-            </DescriptionDifferential>
+              <Text>Estratégia</Text>
+              <DescriptionDifferential>
+                Somos estudiosos, sabia?E adoramos planejar. É assim que
+                descobrimos quais os melhores caminhos para alcançar o seu
+                objetivo.
+              </DescriptionDifferential>
             </DivSuporte>
           </Differential>
 
+          <Differential>
+            <DifferentialLogo>
+              <ImageDifferential src={users} alt="users" />
+            </DifferentialLogo>
+            <DivSuporte>
+              <Text>Integração</Text>
+              <DescriptionDifferential>
+                Juntos conseguimos soluções melhores. Entre uma área e outra, a
+                gente se ajuda e é dessa cooperação que surgem as boas ideias.
+              </DescriptionDifferential>
+            </DivSuporte>
+          </Differential>
         </DifferentialsBox>
       </WhyUsSection>
 
